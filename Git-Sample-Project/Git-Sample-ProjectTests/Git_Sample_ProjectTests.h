348e8412d79a8f98e200fc2c31fa25755be5e3f2
Loganathan <loganathan_balakrishnan@thbs.com>
2013-02-12 12:36:08 +0530

    Initial commit
    I have ceated a Xcode iPhone project with ARC, storyboard and unit test
    case. It's named Git-sample-project

---
+++ Git-Sample-Project/Git-Sample-ProjectTests/Git_Sample_ProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  Git_Sample_ProjectTests.h
+//  Git-Sample-ProjectTests
+//
+//  Created by Loganathan on 12/02/13.
+//  Copyright (c) 2013 Loganathan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Git_Sample_ProjectTests : SenTestCase
+
+@end
---
+++ Git-Sample-Project/Git-Sample-ProjectTests/Git_Sample_ProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  Git_Sample_ProjectTests.m
+//  Git-Sample-ProjectTests
+//
+//  Created by Loganathan on 12/02/13.
+//  Copyright (c) 2013 Loganathan. All rights reserved.
+//
+
+#import "Git_Sample_ProjectTests.h"
+
+@implementation Git_Sample_ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Git-Sample-ProjectTests");
+}
+
+@end
